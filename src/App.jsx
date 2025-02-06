d7d8f788c1a0909bcb97a94dfca313218db7983c
PedroJCAlmeida <pedrojoaocarvalhoalmeida@gmail.com>
2025-02-06 09:26:06 +0000

    Creating,  providing and consuming the context

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header.jsx';
 import Product from './components/Product.jsx';
 import Shop from './components/Shop.jsx';
 import { DUMMY_PRODUCTS } from './dummy-products.js';
+import {CartContext} from './store/shopping-cart-context.jsx'
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -68,6 +69,7 @@ function App() {
 
   return (
     <>
+    <CartContext.Provider value={{items:[]}}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
@@ -79,6 +81,7 @@ function App() {
           </li>
         ))}
       </Shop>
+      </CartContext.Provider>
     </>
   );
 }
